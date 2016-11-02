bc73df0e64ebb3fe3ed5a45effcd50f04189a247
James Wright <james.wright037@gmail.com>
2016-11-02 16:23:50 +0800

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,14 @@ class User < ApplicationRecord
 
   has_many :authentications, :dependent => :destroy
 
-	has_many :interests, through: :user_interests
-	has_many :user_interests
+  has_many :interests, through: :user_interests
+  has_many :user_interests
 
-	has_many :teach_languages, through: :user_teach_languages
-	has_many :user_teach_languages
+  has_many :teach_languages, through: :user_teach_languages
+  has_many :user_teach_languages
 
-	has_many :learn_languages, through: :user_learn_languages
-	has_many :user_learn_languages
+  has_many :learn_languages, through: :user_learn_languages
+  has_many :user_learn_languages
 
 
 

a3a1ca0f5af675a040912daa7459164fa8f4e6df
Benjamin Phang <BenPhang@Benjamins-MacBook-Pro.local>
2016-11-02 16:21:34 +0800

    added video functionality to app

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,11 +5,14 @@ class WelcomeController < ApplicationController
   end
 
   def video
-  	session_id = opentok.create_session.session_id
-  	token = OpenTok::OpenTok.new('45711342', '54525e65d49550a0df0d750a22c915cfbd5260c0').generate_token(session_id)
-  	render :video,  :locals => {
+  	api_key = '45711342'
+  	@opentok= OpenTok::OpenTok.new('45711342', '54525e65d49550a0df0d750a22c915cfbd5260c0')
+  	session_id = @opentok.create_session.session_id
+  	token = @opentok.generate_token(session_id)
+  	render :video,:locals => {
+      :api_key => api_key,
       :session_id => session_id,
       :token => token
-    }
+   }
   end
 end

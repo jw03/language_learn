afe166bf885572ea2b0643596584904ad3f2c405
nextacademy <yithanglee@gmail.com>
2016-11-03 15:14:49 +0800

    fixed issue with nil values and view other profiles

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,13 @@
 class WelcomeController < ApplicationController
   def index
-  	@languages = Language.all
-  	@interests = Interest.all
-  	@user = current_user
-  	@teach_languages =current_user.teach_languages.all
-  	@learn_languages =current_user.learn_languages.all
-  	@user_interest = current_user.interests.all
+  	if signed_in?
+	  	@languages = Language.all
+	  	@interests = Interest.all
+	  	@user = current_user
+	  	@teach_languages = current_user.teach_languages.all
+	  	@learn_languages = current_user.learn_languages.all
+	  	@user_interest = current_user.interests.all
+	  else
+	  end
   end
 end

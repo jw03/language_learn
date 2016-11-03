ce1df6493c8682994bb6d25c6859cb9488e384b3
nextacademy <yithanglee@gmail.com>
2016-11-03 17:28:51 +0800

    added the purchase option and show the purchase coins, not the balance as theres no used coins record yet

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -4,7 +4,7 @@ class PaymentsController < ApplicationController
 
   def new
     @client_token = Braintree::ClientToken.generate
-    @reservation = Reservation.find(params[:id])
+    @chat = Chat.find(params[:id])
     @payment = Payment.new
   end
 
@@ -22,7 +22,16 @@ class PaymentsController < ApplicationController
     if @result.success?
       Payment.create(chat_id: params[:payment][:chat_id], braintree_payment_id: @result.transaction.id, status: @result.transaction.status, fourdigit: @result.transaction.credit_card_details.last_4)
 
-        redirect_to reservation_path(:id => params[:payment][:chat_id]), notice: "Congratulations! Your transaction is successful!"
+      @chat = Chat.find(params[:payment][:chat_id])
+      	if @chat.payments.count == 0
+      	else
+      		@chat.update(is_paid:"1")
+          @host = User.find(@chat.host_id)
+          b = Chat.where(host_id: @chat.host_id, is_paid:1).pluck(:amount)
+          @host.update(total_coins: b.inject(:+))
+      	end
+
+        redirect_to user_path(id: current_user.id), notice: "Congratulations! Your transaction is successful!"
     else
         Payment.create(chat_id: params[:payment][:chat_id], braintree_transaction_id: @result.transaction.id, status: @result.transaction.status, fourdigit: @result.transaction.credit_card_details.last_4)
         flash[:alert] = "Something went wrong while processing your transaction. Please try again!"

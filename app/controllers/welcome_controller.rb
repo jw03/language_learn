a4dfdaab34ce6412e60aacc673d5f40c18db404e
nextacademy <yithanglee@gmail.com>
2016-11-06 18:10:42 +0800

    partial commit

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -11,20 +11,10 @@ class WelcomeController < ApplicationController
 	  	@teach_languages = current_user.teach_languages.all
 	  	@learn_languages = current_user.learn_languages.all
 	  	@user_interest = current_user.interests.all
-      @chats_available = Chat.all.where.not(chat_session: nil)
+      @chats_available = Chat.all.where.not(chat_session: nil).where(guest_id: nil)
 	  else
 	  end
   end
 
-  def video
-  	api_key = ENV["API_KEY"]
-  	@opentok= OpenTok::OpenTok.new(ENV["API_KEY"], ENV["API_SECRET"])
-  	session_id = @opentok.create_session.session_id
-  	token = @opentok.generate_token(session_id)
-  	render :video,:locals => {
-      :api_key => api_key,
-      :session_id => session_id,
-      :token => token
-   }
-  end
+
 end

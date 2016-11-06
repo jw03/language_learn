49c5fe03cf2597e8113c8d360ecdf4be0dda2bda
nextacademy <yithanglee@gmail.com>
2016-11-06 20:47:48 +0800

    enhanced association

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,11 +8,9 @@ class User < ApplicationRecord
   has_many :interests, through: :user_interests
   has_many :user_interests
 
-  has_many :teach_languages, through: :user_teach_languages
-  has_many :user_teach_languages
+  has_and_belongs_to_many :teach_languages
 
-  has_many :learn_languages, through: :user_learn_languages
-  has_many :user_learn_languages
+  has_and_belongs_to_many :learn_languages
 
 # validations for the correct input format
   validates :email, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i}, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -52,6 +52,15 @@ ActiveRecord::Schema.define(version: 20161103090949) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "learn_languages_users", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "learn_language_id"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
+    t.index ["learn_language_id"], name: "index_learn_languages_users_on_learn_language_id", using: :btree
+    t.index ["user_id"], name: "index_learn_languages_users_on_user_id", using: :btree
+  end
+
   create_table "payments", force: :cascade do |t|
     t.integer  "chat_id"
     t.string   "braintree_payment_id"
@@ -75,6 +84,15 @@ ActiveRecord::Schema.define(version: 20161103090949) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "teach_languages_users", force: :cascade do |t|
+    t.integer  "teach_language_id"
+    t.integer  "user_id"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
+    t.index ["teach_language_id"], name: "index_teach_languages_users_on_teach_language_id", using: :btree
+    t.index ["user_id"], name: "index_teach_languages_users_on_user_id", using: :btree
+  end
+
   create_table "user_interests", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "interest_id"
@@ -82,24 +100,6 @@ ActiveRecord::Schema.define(version: 20161103090949) do
     t.datetime "updated_at",  null: false
     t.index ["interest_id"], name: "index_user_interests_on_interest_id", using: :btree
     t.index ["user_id"], name: "index_user_interests_on_user_id", using: :btree
-  end
-
-  create_table "user_learn_languages", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "learn_language_id"
-    t.datetime "created_at",        null: false
-    t.datetime "updated_at",        null: false
-    t.index ["learn_language_id"], name: "index_user_learn_languages_on_learn_language_id", using: :btree
-    t.index ["user_id"], name: "index_user_learn_languages_on_user_id", using: :btree
-  end
-
-  create_table "user_teach_languages", force: :cascade do |t|
-    t.integer  "teach_language_id"
-    t.integer  "user_id"
-    t.datetime "created_at",        null: false
-    t.datetime "updated_at",        null: false
-    t.index ["teach_language_id"], name: "index_user_teach_languages_on_teach_language_id", using: :btree
-    t.index ["user_id"], name: "index_user_teach_languages_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
